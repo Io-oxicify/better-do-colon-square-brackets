d85bfb504835cc5ae60cf00e9077cdff4c43b2e1
Oxic <pipatalt@gmail.com>
2026-05-25 17:39:45 +0700

    add confirm

--- better-do-:]/popup.js
+++ better-do-:]/popup.js
@@ -7,5 +7,15 @@ chrome.storage.sync.get(["enabled"], (data) => {
 
 // update state
 toggle.addEventListener("change", () => {
+  if (!toggle.checked) {
+    const confirmDisable = confirm("Would you like to disable this effect? (Requires page reload)");
+    
+    if (!confirmDisable) {
+      toggle.checked = true; 
+      return;
+    }
+  }
+
+  // i think this is save?
   chrome.storage.sync.set({ enabled: toggle.checked });
 });
